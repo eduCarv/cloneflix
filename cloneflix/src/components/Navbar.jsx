3da510dc7a44738c841c4788c95e445de9fd32a8
eduCarv <edu.ascarvalho@gmail.com>
2024-04-16 23:03:22 -0300

    cria hero e seus botoes da home

--- cloneflix/src/components/Navbar.jsx
+++ cloneflix/src/components/Navbar.jsx
@@ -18,11 +18,10 @@ export default function Navbar({ isScrolled }) {
   const [inputHover, setInputHover] = useState(false);
 
   const navigate = useNavigate();
-   
+
   onAuthStateChanged(firebaseAuth, (currentUser) => {
     if (!currentUser) navigate("/login");
   });
-
 
   return (
     <Container>
@@ -32,7 +31,7 @@ export default function Navbar({ isScrolled }) {
             <img src={logo} alt="logo" />
           </div>
           <ul className="links flex">
-            {links.map(({name, link}) => {
+            {links.map(({ name, link }) => {
               return (
                 <li key={name}>
                   <Link to={link}>{name}</Link>
@@ -62,7 +61,7 @@ export default function Navbar({ isScrolled }) {
               }}
             />
           </div>
-          <button onClick={()=> signOut(firebaseAuth)}>
+          <button onClick={() => signOut(firebaseAuth)}>
             <FaPowerOff />
           </button>
         </div>
@@ -72,92 +71,92 @@ export default function Navbar({ isScrolled }) {
 }
 
 const Container = styled.div`
-    .scrolled {
-        background-color: black;
+  .scrolled {
+    background-color: black;
+  }
+  nav {
+    position: sticky;
+    top: 0;
+    height: 6.5rem;
+    width: 100%;
+    justify-content: space-between;
+    position: fixed;
+    z-index: 2;
+    padding: 0 4rem;
+    align-items: center;
+    transition: all 0.3s ease-in-out;
+    .left {
+      gap: 2rem;
+      .brand {
+        img {
+          height: 4rem;
+        }
+      }
+      .links {
+        list-style-type: none;
+        gap: 2rem;
+        li {
+          a {
+            color: white;
+            text-decoration: none;
+            transition: all 0.2s ease-in-out;
+            &:hover {
+              opacity: 0.7;
+            }
+          }
+        }
+      }
     }
-    nav {
-        position: sticky;
-        top: 0;
-        height: 6.5rem;
-        width: 100%;
-        justify-content: space-between;
-        position: fixed;
-        z-index: 2;
-        padding: 0 4rem;
+    .right {
+      gap: 1rem;
+      button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        &:focus {
+          outline: none;
+        }
+        svg {
+          color: #f34242;
+          font-size: 1.2rem;
+        }
+      }
+      .search {
+        display: flex;
         align-items: center;
-        transition: all .3s ease-in-out;
-        .left {
-            gap: 2rem;
-            .brand {
-                img {
-                    height: 4rem;
-                }
-            }
-            .links {
-                list-style-type: none;
-                gap: 2rem;
-                li {
-                    a {
-                        color: white;
-                        text-decoration: none;
-                        transition: all .2s ease-in-out;
-                        &:hover {
-                            opacity: .7;
-                        }
-                    }
-                }
-            }
+        justify-content: center;
+        gap: 0.4rem;
+        padding: 0.2rem;
+        padding-left: 0.5rem;
+        button {
+          background-color: transparent;
+          svg {
+            color: white;
+          }
         }
-        .right {
-            gap: 1rem;
-            button {
-                background-color: transparent;
-                border: none;
-                cursor: pointer;
-                &:focus {
-                    outline: none;
-                }
-                svg {
-                    color: #f34242;
-                    font-size: 1.2rem;
-                }
-            }
-            .search {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                gap: .4rem;             
-                padding: .2rem;
-                padding-left: .5rem;
-                button {
-                    background-color: transparent;
-                    svg {
-                        color: white;
-                    }
-                }
-                input {
-                    width: 0;
-                    opacity: 0;
-                    visibility: hidden;
-                    transition: all 0.2s ease-in-out;
-                    background-color: transparent;
-                    border: none;
-                    color: white;
-                    &:focus {
-                        outline: none;
-                    }
-                }
-            }
-            .show-search {
-                border: 1px solid white;
-                background-color: rgba(0,0,0, 0.6);
-                input {
-                    width: 100%;
-                    opacity: 1;
-                    visibility: visible;
-                    padding: 0.3rem;
-                }
-            }
+        input {
+          width: 0;
+          opacity: 0;
+          visibility: hidden;
+          transition: all 0.2s ease-in-out;
+          background-color: transparent;
+          border: none;
+          color: white;
+          &:focus {
+            outline: none;
+          }
         }
+      }
+      .show-search {
+        border: 1px solid white;
+        background-color: rgba(0, 0, 0, 0.6);
+        input {
+          width: 100%;
+          opacity: 1;
+          visibility: visible;
+          padding: 0.3rem;
+        }
+      }
     }
+  }
 `;
--- cloneflix/src/pages/Netflix.jsx
+++ cloneflix/src/pages/Netflix.jsx
@@ -1,17 +1,90 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
+import backgroundImage from "../assets/home.webp";
+import MovieLogo from "../assets/homeTitle.webp";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import { FaPlay } from "react-icons/fa";
+import styled from "styled-components";
 
 export default function Netflix() {
   const [isScrolled, setIsScrolled] = useState(false);
 
+  /**Ativa ou desativa a classe que vai deixar o nav opaco. */
   window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    setIsScrolled(window.scrollY === 0 ? false : true);
     return () => (window.onscroll = null);
   };
 
   return (
-    <div>
+    <Container>
       <Navbar isScrolled={isScrolled} />
-    </div>
+      <div className="hero">
+        <img
+          src={backgroundImage}
+          alt="Plano de fundo"
+          className="background-image"
+        />
+        <div className="container">
+          <div className="logo">
+            <img src={MovieLogo} alt="Capa do Filme" />
+          </div>
+          <div className="buttons flex">
+            <button className="flex j-center a-center"><FaPlay />Assistir</button>
+            <button className="flex j-center a-center"><AiOutlineInfoCircle /> Mais informações</button>
+          </div>
+        </div>
+      </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  background-color: black;
+  .hero {
+    position: relative;
+    .background-image {
+      filter: brightness(60%);
+    }
+    img {
+      height: 100vh;
+      width: 100vw;
+      object-fit: contain;
+    }
+    .container {
+      position: absolute;
+      bottom: 5rem;
+      .logo {
+        img {
+          width: 100%;
+          height: 100%;
+          margin-left: 5rem;
+        }
+      }
+      .buttons {
+        margin: 5rem;
+        gap: 2rem;
+        button {
+          font-size: 1.4rem;
+          gap: 1rem;
+          padding: 0.5rem;
+          padding-left: 2rem;
+          padding-right: 2.4rem;
+          border-radius: 0.2rem;
+          border: none;
+          cursor: pointer;
+          transition: all .3s ease-in-out;
+          &:hover {
+            opacity: 0.8;
+          }
+          &:nth-of-type(2) {
+            background-color: rgba(109,109,110,0.7);
+            color: white;
+            svg {
+              font-size: 1.8rem;
+            }
+          }
+        }
+      }
+    }
+  }
+`;
